9ba210fdd87049773f036b7fe8ab1af513cfb622
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-05 14:34:36 +0300

    ""

---
+++ Student_Reg.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from datetime import date
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+from tkinter.ttk import Combobox
+import openpyxl, xlrd
+from openpyxl import Workbook
+import pathlib
+
+root = Tk()
+root.title("Student Registration System")
+root.geometry("1250x700+210+100")
+root.config()
+
+root.mainloop()
