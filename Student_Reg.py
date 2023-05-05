ccc22ac6f6edf634291a95a29dbf81ff454db329
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-05 19:21:36 +0300

    ""

--- Student_Reg.py
+++ Student_Reg.py
@@ -8,6 +8,7 @@ from tkinter.ttk import Combobox
 import openpyxl, xlrd
 from openpyxl import Workbook
 import pathlib
+
 background = "#06283D"
 framebg = "#EDEDED"
 framefg = "#06283D"
@@ -40,18 +41,15 @@ else:
 
 # Top Frames
 Label(root, text="Email:mutwiriroy63@gmail.com", width=10, height=3, bg="#f0687c", anchor='e').pack(side=TOP, fill=X)
-Label(root, text="STUDENT REGISTRATION", width=10, height=2, bg="#c36464", fg="white", font="arial 20 bold").pack(side=TOP, fill=X)
+Label(root, text="STUDENT REGISTRATION", width=10, height=2, bg="#c36464", fg="white", font="arial 20 bold").pack(
+    side=TOP, fill=X)
 
 # Search Box To Update
 Search = StringVar()
 Entry(root, textvariable=Search, width=15, bd=2, font="arial 20").place(x=820, y=70)
 imageicon3 = PhotoImage(file="/home/anon/PycharmProjects/Login_Sys_Tkinter./Images-20230505T155526Z-001"
                              "/Images/search.png")
-
+Srch = Button(root, text="Search", compound=LEFT, image=imageicon3, width=123, bg='#68ddfa', font="arial 13 bold")
+Srch.place(x=1060, y=66)
 
 root.mainloop()
-
-
-
-
-
