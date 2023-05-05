97a3eba82de6b3e01834d2951699cb78cf21c2f7
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-05 18:29:06 +0300

    changed background

--- Student_Reg.py
+++ Student_Reg.py
@@ -17,6 +17,25 @@ root.title("Student Registration System")
 root.geometry("1250x700+210+100")
 root.config(bg=background)
 
+file = pathlib.Path("Student_data.xlsx")
+if file.exists():
+    pass
+else:
+    file = Workbook()
+    sheet = file.active
+    sheet['A1'] = "Registration No."
+    sheet['B1'] = ""
+    sheet['C1'] = ""
+    sheet['D1'] = ""
+    sheet['E1'] = ""
+    sheet['F1'] = ""
+    sheet['G1'] = ""
+    sheet['H1'] = ""
+    sheet['I1'] = ""
+    sheet['J1'] = ""
+    sheet['K1'] = ""
+    sheet['L1'] = ""
+
 root.mainloop()
 
 
